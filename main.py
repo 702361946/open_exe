ba77eb1d03243cba1287fda405d9a32ff632b8b0
702361946 <702361946@qq.com>
2025-05-01 23:19:16 +0800

    tk界面
    加入tk并重命名exe_all为config

--- main.py
+++ main.py
@@ -1,48 +1,118 @@
 #  Copyright (c) 2025.
 #  702361946@qq.com(https://github.com/702361946)
-import os
+import subprocess
+import time
+import tkinter.messagebox as mbox
 
 from dependency import Log, json, log_path
+from dependency.modules._tkinter import Window
 
 log = Log(
     "main",
     log_output_to_file_path=f"{log_path}main.log",
 )
-exe_all = json.load("exe_all")
-if exe_all is False:
-    log.error("exe_all.json not found")
-    exe_all: dict[str, dict[str, str | None]] = {
+config = json.load("config")
+if config is False:
+    log.error("config.json not found")
+    config: dict[str, dict[str, str | None]] = {
         "name_compare": {
 
         },
+        "Force Start": False,
         "demo": {
             "path": None,
             "cmd": None
         }
     }
-    json.dump(exe_all, "exe_all")
+    json.dump(config, "config")
 
 
-def open_exe(exe_path: str, cmd: str = None):
+def open_exe(exe_path: str, cmd: str = None, box: bool = False, sleep: float = 1):
     log.info(f'open_exit: "{exe_path}" {cmd}')
+    if_open = True
     if exe_path is None:
         log.error("exe_path is None")
-        return
+        if_open = False
     if cmd is None:
         cmd = ""
-    os.system(f'"{exe_path}" {cmd}')
+
+    if if_open:
+        a = subprocess.Popen(f'"{exe_path}" {cmd}', shell=True)
+
+        time.sleep(sleep)
+
+        if a.poll() is None:
+            log.info(f'open_exit: "{exe_path}" {cmd} success')
+            if box:
+                mbox.showinfo("提示", "启动成功")
+        else:
+            log.info(f'open_exit: "{exe_path}" {cmd} exit')
+            if box:
+                mbox.showerror("提示", "启动失败")
+    else:
+        log.info(f'open_exit: "{exe_path}" {cmd} exit')
+        if box:
+            mbox.showerror("提示", "启动失败")
+
+
+def compare(name: str, return_as_is: bool = None):
+    if return_as_is is None:
+        return_as_is = config["Force Start"]
+
+    if name in config.keys():
+        t = name
+
+    elif name in config["name_compare"].keys():
+        name = config["name_compare"][name]
+        t = name
+    else:
+        log.info(f"not compare: {name}")
+        t = None
+
+    if t == "name_compare":
+        log.info("compare error: name compare == name_compare")
+        return None, None
+
+    elif t in config.keys():
+        return config[t]["path"], config[t]["cmd"]
+
+    else:
+        log.info(f"not compare: {t}")
+        if return_as_is:
+            return name, None
+        return None, None
+
+
+def run():
+    win = Window(
+        title="Main",
+        geometry="150x50",
+        resizable=(False, False),
+    )
+    win.entry(
+        "input_path",
+        default_text="在此输入应用程序名",
+        pack_mode="place",
+    )
+    win.button(
+        "open",
+        text="确认",
+        pack_mode="place",
+        command=lambda: open_exe(
+            *compare(
+                win.get(
+                    "input_path",
+                    "entry",
+                    entry_return_type="get"
+                )
+            ),
+            box=True
+        ),
+        y=20
+    )
+
+    win.run()
 
 
 if __name__ == '__main__':
-    while True:
-        t = input("输入应用程序名")
-        if t == "exit":
-            break
-        elif t == "name_compare":
-            print("此为对照表,不可启动")
-            continue
-        elif t in exe_all.keys():
-            open_exe(exe_all[t]["path"], exe_all[t]["cmd"])
-        elif t in exe_all["name_compare"].keys():
-            name = exe_all["name_compare"][t]
-            open_exe(exe_all[name]["path"], exe_all[name]["cmd"])
+    run()
